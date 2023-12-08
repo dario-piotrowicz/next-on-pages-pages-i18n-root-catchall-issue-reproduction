c42ed16daf16473642793404c89797a45da27779
Dario Piotrowicz <dario@cloudflare.com>
2023-12-08 09:53:05 +0000

    reproduce issue

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -8,5 +8,5 @@ export const config = {
 }
 
 export default async function handler(req: NextRequest) {
-  return new Response(JSON.stringify({ name: 'John Doe' }))
+  return new Response('Hello world')
 }
